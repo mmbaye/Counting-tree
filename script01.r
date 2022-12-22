e93b166be075adcd1f96ab904c4ab05f4821f76f
Modou Mbaye <modou2812.mbaye@ucad.edu.sn>
2022-12-22 06:20:59 +0100

    we add some changes to all fiels

---
+++ script01.r
@@ -0,0 +1,11 @@
+suppressPackageStartupMessages({
+  library(tmap)
+  library(sf)
+  library(mapview)
+  library(grid)
+  library(units)
+  library(tidyverse)
+  library(raster)
+  library(leaflet)
+  library(mapedit)
+})
